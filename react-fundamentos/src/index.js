29002f6a6aa06bf59e0376e225bfb05ecfcdb5c9
Mariotsx <zacek3082@gmail.com>
2020-05-09 15:03:48 -0500

    Se añade segundo nombre

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name='Mario Quiroz Guzman';
+const name='Mario Edgar Quiroz Guzman';
 const App = <h1>Hola React { name }</h1>
 
 ReactDOM.render(App,document.getElementById('root'));
