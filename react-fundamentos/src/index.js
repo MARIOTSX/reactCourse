3efe4fc893a6438e243e5563c08b7f07217ce9f3
Mariotsx <zacek3082@gmail.com>
2020-05-13 15:48:33 -0500

    cambios Clase

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 const name='Mario Edgar Quiroz Guzman';
 
+/*
 const user1 ={
   name: 'Gerardo Quiroz',
   age: 28,
@@ -12,7 +13,45 @@ const user1 ={
 function getInfo(user){
   return  `Hola mi nombre es ${user.name} y soy de ${user.country}`;
 }
+*/
 
-const App = <h1>Hola React { name }, tengo { user1.age} , y el doble de mi edad es: {user1.age * 2}, y además { getInfo(user1) } </h1>
+class TarjetaFruta extends React.Component{
+ 
+ constructor(){
+   super();
 
-ReactDOM.render(App,document.getElementById('root'));
+   this.state = {
+     cantidad: 10
+   }
+ }
+
+  render(){
+    return (
+      <div>
+      <h3>{this.props.name}</h3>
+      <hr/>
+    <div> Cantidad: {  this.state.cantidad }</div>
+      <button onClick={()=>{
+        this.setState({cantidad: this.state.cantidad+1})
+      }
+      }>
+        Agregar
+      </button>
+      <p>{this.props.price}</p>
+      <p>This is the real shit</p>
+    </div>
+    )
+  }
+}
+   
+
+const App = () => (
+    <div>
+      <TarjetaFruta name='Sandia' price={4.00}/>
+      <TarjetaFruta name='Melón'/>
+      <TarjetaFruta name='Kiwi'/>
+    </div>
+) ;
+//<h1>Hola React { name }, tengo { user1.age} , y el doble de mi edad es: {user1.age * 2}, y además { getInfo//(user1) } </h1>
+
+ReactDOM.render(<App />,document.getElementById('root'));
